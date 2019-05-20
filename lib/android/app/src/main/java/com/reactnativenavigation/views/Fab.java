92fa4a79e8cb55a86b59ab8228ac91819a26324f
thecodrr <enkaboot@gmail.com>
2019-05-21 00:41:32 +0500

    fix minor bug

--- lib/android/app/src/main/java/com/reactnativenavigation/views/Fab.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/views/Fab.java
@@ -40,9 +40,8 @@ public class Fab extends FloatingActionButton implements FabAnimator {
     public void applyIcon(String icon, String text, int fontSize, @ColorInt int textColor) {
         if (text != null && fontSize != 0 && textColor != 0 && icon == null) {
             Typeface font = Typeface.createFromAsset(getContext().getAssets(), "fonts/Roboto.ttf");
-            int scaledSize = getContext().getResources().getDimensionPixelSize(fontSize);
             TextDrawable drawable = TextDrawable.builder().beginConfig().textColor(textColor).useFont(font)
-                    .fontSize(scaledSize).endConfig().buildRect(text, Color.TRANSPARENT);
+                    .fontSize(fontSize).endConfig().buildRect(text, Color.TRANSPARENT);
             setImageDrawable(drawable);
         } else
             new ImageLoader().loadIcons(getContext(), Collections.singletonList(icon),
